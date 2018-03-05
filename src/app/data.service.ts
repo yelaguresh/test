17eda1d345bac3ee55ad40e2eb9163aa8ea264d4
Yelagulesh Deshpande <yelagulesh.deshpande@crown.com>
2018-03-05 16:46:46 -0500

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class DataService {
+  private goals=new BehaviorSubject<any>(['The intial goal','Second goal']);
+  goal=this.goals.asObservable();
+  constructor() { }
+  changeGoal(goal)
+  {
+    this.goals.next(goal);
+  }
+
+}
